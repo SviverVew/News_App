03f8e3b74081a85ae58fb33bf07d0934c9c8d548
NGÔ THANH THIÊN <skycorn4@gmail.com>
2024-02-21 10:02:26 +0700

    cai loz

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // minh ne
+        //cai loz me m
     }
 }
