bb3337e9c2495580649e10c0e07fbb2bcea983e0
Nguyễn Nhật Minh <22dh112160@st.huflit.edu.vn>
2024-02-29 06:06:17 +0700

    minh update layout lan 1

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -11,11 +11,6 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.newsapp.fragments.NewsFragment;
-import com.example.newsapp.fragments.TienichFragment;
-import com.example.newsapp.fragments.TrendFragment;
-import com.example.newsapp.fragments.VideoFragment;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,48 +40,12 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Làm đẹp"));
         tabLayout.addTab(tabLayout.newTab().setText("Sức khỏe"));
         tabLayout.addTab(tabLayout.newTab().setText("Du lịch"));
-        //BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationViewHome);
-        //bottomNavigationView.setOnItemSelectedListener(onItemSelectedListener());
-        //setFragment(new NewsFragment());
+
     }
 
-    void setFragment(Fragment newFragment) {
-        // Create new fragment and transaction
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        // Replace whatever is in the fragment_container view with this fragment,
-        // and add the transaction to the back stack if needed
-        transaction.replace(R.id.fragmentContainerView, newFragment);
-        transaction.addToBackStack(null);
-        // Commit the transaction
-        transaction.commit();
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.topmenu, menu);
         return true;
     }
-    /*private NavigationBarView.OnItemSelectedListener onItemSelectedListener() {
-        return new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.idtintuc) {
-                    setFragment(new NewsFragment());
-                    myToolbar.setTitle("Chats");
-                }
-                if(item.getItemId() == R.id.idvideo) {
-                    setFragment(new VideoFragment());
-                    myToolbar.setTitle("Calls");
-                }
-                if(item.getItemId() == R.id.idxuhuong) {
-                    setFragment(new TrendFragment());
-                    myToolbar.setTitle("People");
-                }
-                if(item.getItemId() == R.id.idtienich) {
-                    setFragment(new TienichFragment());
-                    myToolbar.setTitle("Stories");
-                }
-                return true;
-            }
-        };
-    }*/
 }
