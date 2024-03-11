60f451ba60b75329b21862148b966f870962d89d
NGÔ THANH THIÊN <skycorn4@gmail.com>
2024-03-11 12:19:38 +0700

    update

---
+++ app/src/main/java/com/example/newsapp/Show_Context_Activity.java
@@ -0,0 +1,14 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Show_Context_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_context);
+    }
+}
