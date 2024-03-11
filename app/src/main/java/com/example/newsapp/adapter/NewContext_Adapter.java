26a94c729a4a60f531a33eb87b0a36f29b81b04d
NGÔ THANH THIÊN <skycorn4@gmail.com>
2024-03-11 12:21:30 +0700

    update

--- app/src/main/java/com/example/newsapp/adapter/NewContext_Adapter.java
+++ app/src/main/java/com/example/newsapp/adapter/NewContext_Adapter.java
@@ -1,74 +1,42 @@
 package com.example.newsapp.adapter;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.newsapp.LoginActivity;
 import com.example.newsapp.R;
+import com.example.newsapp.Show_Context_Activity;
 import com.example.newsapp.testmodel.News;
 
 import java.util.ArrayList;
-//
-//public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.ViewHolder> {
-//    ArrayList<News> arr_News;
-//    Activity Context;
-//
-//    public NewContext_Adapter(ArrayList<News> arr_News) {
-//        this.arr_News = arr_News;
-//    }
-//
-//    public NewContext_Adapter(ArrayList<News> arr_News, Activity context) {
-//        this.arr_News = arr_News;
-//        Context = context;
-//    }
-//
-//    @NonNull
-//    @Override
-//    public NewContext_Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        LayoutInflater inflater = LayoutInflater.from(Context);
-//        View news = inflater.inflate(R.layout.item_news,parent,false);
-//        return new ViewHolder(news);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull NewContext_Adapter.ViewHolder holder, int position) {
-//        News news = arr_News.get(position);
-//        holder.image.setImageResource(news.getImage());
-//        holder.title.setText(news.getTitle()+"");
-//        holder.user.setText(news.getUser());
-//        holder.time.setText(news.getTime()+"");
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return arr_News.size();
-//    }
-//
-//    public class ViewHolder extends RecyclerView.ViewHolder{
-//        ImageView image;
-//        TextView title,user,time;
-//        public ViewHolder(@NonNull View itemview){
-//            super(itemview);
-//            image =itemview.findViewById(R.id.item_news_image);
-//            title = itemview.findViewById(R.id.item_main_title);
-//            user = itemview.findViewById(R.id.item_main_poster);
-//            time = itemview.findViewById(R.id.item_main_time);
-//        }
-//    }
-//}
 
 public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.ViewHolder> {
+
     private ArrayList<News> arr_News;
+    Context context;
+
+    public NewContext_Adapter(ArrayList<News> arr_News, Context context) {
+        this.arr_News = arr_News;
+        this.context = context;
+    }
+
 
     public NewContext_Adapter(ArrayList<News> arr_News) {
         this.arr_News = arr_News;
-    }
+   }
 
     @NonNull
     @Override
@@ -84,6 +52,20 @@ public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.
         holder.title.setText(news.getTitle());
         holder.user.setText(news.getUser());
         holder.time.setText(String.valueOf(news.getTime()));
+        holder.category.setText(news.getCategory());
+        holder.view.setText(news.getView());
+//       String res = news.getUser().toString();
+        //add click listener
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(v.getContext(),"mày đang chọn bài báo số " + res,Toast.LENGTH_LONG).show();
+                  Intent i = new Intent(v.getContext(), Show_Context_Activity.class);
+                  i.putExtra("data", news);
+                  v.getContext().startActivity(i);
+                  Log.d(TAG, "onClick: " + news.getContext());
+            }
+        });
     }
 
     @Override
@@ -93,7 +75,7 @@ public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView image;
-        TextView title, user, time;
+        TextView title, user, time, view, category;
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
@@ -101,6 +83,8 @@ public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.
             title = itemView.findViewById(R.id.item_main_title);
             user = itemView.findViewById(R.id.item_main_poster);
             time = itemView.findViewById(R.id.item_main_time);
+            view = itemView.findViewById(R.id.show_news_view);
+            category = itemView.findViewById(R.id.show_news_category);
         }
     }
 }
