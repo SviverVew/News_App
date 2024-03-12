56f07cc39d60c2cf2b0cce1b12dd04a75599675d
Nguyễn Nhật Minh <22dh112160@st.huflit.edu.vn>
2024-03-13 01:02:05 +0700

    search

---
+++ app/src/main/java/com/example/newsapp/Search_Activity.java
@@ -0,0 +1,107 @@
+package com.example.newsapp;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+
+import com.example.newsapp.adapter.NewContext_Adapter;
+import com.example.newsapp.testmodel.News;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Search_Activity extends AppCompatActivity {
+    NewContext_Adapter news;
+    RecyclerView recyclerView;
+    FirebaseFirestore db;
+    MaterialToolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        toolbar = findViewById(R.id.main_search);
+        setSupportActionBar(toolbar);
+        setTitle("");
+        recyclerView = findViewById(R.id.rcvnews);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+        // Gọi hàm getDataFromFirestore() để lấy dữ liệu từ Firestore
+        getDataFromFirestore();
+
+        SearchView searchView = (SearchView) findViewById(R.id.mnsearches);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                news.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                news.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+
+
+
+    private void getDataFromFirestore() {
+        db = FirebaseFirestore.getInstance();
+        db.collection("news")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<News> newsList = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // Lấy dữ liệu của tài liệu
+                                Map<String, Object> data = document.getData();
+                                // Tạo object News từ dữ liệu Firestore
+                                News objNews = new News();
+                                objNews.setImage(R.drawable.google);
+                                objNews.setTitle(data.get("news_Title").toString());
+                                objNews.setContext(data.get("news_Context").toString());
+                                objNews.setUser(data.get("news_ID").toString());
+                                objNews.setTime(data.get("news_time").toString());
+                                //objNews.setView(Integer.parseInt(data.get("news_View").toString()));
+                                newsList.add(objNews);
+                            }
+
+                            // Khởi tạo adapter và set dữ liệu vào RecyclerView
+                            news = new NewContext_Adapter(newsList);
+                            recyclerView.setAdapter(news);
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+}
--- app/src/main/java/com/example/newsapp/adapter/NewContext_Adapter.java
+++ app/src/main/java/com/example/newsapp/adapter/NewContext_Adapter.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,61 +15,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.newsapp.R;
 import com.example.newsapp.testmodel.News;
 
+import java.io.Serializable;
 import java.util.ArrayList;
-//
-//public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.ViewHolder> {
-//    ArrayList<News> arr_News;
-//    Activity Context;
-//
-//    public NewContext_Adapter(ArrayList<News> arr_News) {
-//        this.arr_News = arr_News;
-//    }
-//
-//    public NewContext_Adapter(ArrayList<News> arr_News, Activity context) {
-//        this.arr_News = arr_News;
-//        Context = context;
-//    }
-//
-//    @NonNull
-//    @Override
-//    public NewContext_Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        LayoutInflater inflater = LayoutInflater.from(Context);
-//        View news = inflater.inflate(R.layout.item_news,parent,false);
-//        return new ViewHolder(news);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull NewContext_Adapter.ViewHolder holder, int position) {
-//        News news = arr_News.get(position);
-//        holder.image.setImageResource(news.getImage());
-//        holder.title.setText(news.getTitle()+"");
-//        holder.user.setText(news.getUser());
-//        holder.time.setText(news.getTime()+"");
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return arr_News.size();
-//    }
-//
-//    public class ViewHolder extends RecyclerView.ViewHolder{
-//        ImageView image;
-//        TextView title,user,time;
-//        public ViewHolder(@NonNull View itemview){
-//            super(itemview);
-//            image =itemview.findViewById(R.id.item_news_image);
-//            title = itemview.findViewById(R.id.item_main_title);
-//            user = itemview.findViewById(R.id.item_main_poster);
-//            time = itemview.findViewById(R.id.item_main_time);
-//        }
-//    }
-//}
+import java.util.List;
+public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.ViewHolder> implements Filterable {
+    private List<News> arr_News;
+    private List<News> getArr_Newsold;
+    public NewContext_Adapter(List<News> arr_News) {
 
-public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.ViewHolder> {
-    private ArrayList<News> arr_News;
-
-    public NewContext_Adapter(ArrayList<News> arr_News) {
         this.arr_News = arr_News;
+        this.getArr_Newsold = arr_News;
     }
 
     @NonNull
@@ -91,6 +48,7 @@ public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.
         return arr_News.size();
     }
 
+
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView image;
         TextView title, user, time;
@@ -102,5 +60,37 @@ public class NewContext_Adapter extends RecyclerView.Adapter<NewContext_Adapter.
             user = itemView.findViewById(R.id.item_main_poster);
             time = itemView.findViewById(R.id.item_main_time);
         }
+    }
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String strSearch = constraint.toString();
+                if(strSearch.isEmpty())
+                {
+                    arr_News = getArr_Newsold;
+                }
+                else {
+                    List<News> arrayList = new ArrayList<>();
+                    for (News news : getArr_Newsold){
+                        if(news.getTitle().toLowerCase().contains(strSearch.toLowerCase()))
+                        {
+                            arrayList.add(news);
+                        }
+                    }
+                    arr_News = arrayList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = arr_News;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                arr_News = (List<News>) results.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
